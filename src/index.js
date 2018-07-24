2df2bec604c857bf77ecb6551fe95729c7c7d7c8
Nguyen Hieu <nguyenhuuthihieu@gmail.com>
2018-07-24 16:14:58 +0700

    create oas-front-end

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+ReactDOM.render(<App />, document.getElementById('root'));
